f3cd2aa49d4c1782a78d05299c7571e8b9370092
박지훈 <hoon.park@wavebridge.com>
2023-02-03 18:12:50 +0900

    custom enum class
    - ADD return name list func
    - ADD return value list func
    - ADD check is same with value func

---
+++ app/constants/custom_class.py
@@ -0,0 +1,30 @@
+"""
+    공통으로 사용되는 custom classes
+"""
+from typing import List
+from enum import Enum
+
+
+class CustomEnum(Enum):
+    @classmethod
+    def name_list(cls) -> List:
+        """return name list"""
+        return [data.name for data in cls]
+
+    @classmethod
+    def value_list(cls) -> List:
+        """return value list"""
+        return [data.value for data in cls]
+
+    def __eq__(self, other) -> bool:
+        """check is equal with other value"""
+        if isinstance(other, CustomEnum):
+            return self is other
+        elif self.value == other:
+            return True
+        return False
+
+
+class Test(CustomEnum):
+    A = "a"
+    B = "b"
